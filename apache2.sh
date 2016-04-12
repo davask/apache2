13c06bca0bd260df4fff09398436e77f3c8893ca
davask <davask.42@gmail.com>
2016-04-12 16:33:19 +0200

    update default log dir

--- apache2.sh
+++ apache2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # declare extra apache log dir
-dwl_func_mkdir ${DWL_USER_DIR}/log ${dir}
+dwl_func_mkdir ${DWL_USER_DIR}/log apache2
 
 service apache2 start
 echo "apache2 Initialized";
