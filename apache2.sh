68793065da72a0af238c0cf4630c751bc8908501
davask <davask.42@gmail.com>
2016-04-12 09:54:59 +0200

    rm ln

--- apache2.sh
+++ apache2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 chown -R ${DWL_USER_NAME}:${APACHE_RUN_GROUP} ${DWL_USER_DIR_TMP}
-ln -s ${DWL_USER_DIR}/${DWL_APP_DIR} /var/www/html
 service apache2 start
 echo "apache2 Initialized";
