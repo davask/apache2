53cbdca227c15631e6f6ec1187ca3abc917c760f
davask <davask.42@gmail.com>
2016-09-27 22:28:19 +0200

    Automatic update 2.4-u16.04

--- tmp/dwl/init.sh
+++ tmp/dwl/init.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+. /tmp/dwl/envvar.sh
 . /tmp/dwl/user.sh
 . /tmp/dwl/ssh.sh
 echo ">> Ubuntu initialized";
@@ -11,5 +12,8 @@ echo ">> dwl conf activated";
 
 . /tmp/dwl/apache2.sh
 echo ">> apache2 initialized";
+
+. /tmp/dwl/permission.sh
+echo ">> permission assigned";
 
 . /tmp/dwl/keeprunning.sh
