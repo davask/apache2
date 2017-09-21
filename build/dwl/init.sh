7775ba713f97997d0f4c409cd67b524cb9711357
davask <contact@davaskweblimited.com>
2017-09-22 01:23:23 +0200

    Automatic update 2.4-u16.04

--- build/dwl/init.sh
+++ build/dwl/init.sh
@@ -24,5 +24,13 @@ echo ">> Virtualhost generic update";
 echo ">> Apache2 initialized";
 
 . ${dwlDir}/custom.sh
+echo ">> custom initialized";
+
+# . ${dwlDir}/senmail.sh
+# sendmail is only available from davask/d-php*
+if [ "`dpkg --get-selections | awk '{print $1}' | grep sendmail$ | wc -l`" == "1" ]; then
+  sudo service sendmail start;
+  echo ">> Sendmail initialized";
+fi
 
 tail -f /dev/null;
